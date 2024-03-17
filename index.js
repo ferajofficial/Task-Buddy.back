61369fff8d0c50cebe520662713dfbc9555439c7
Sk Ferajuddin <110988843+ferajofficial@users.noreply.github.com>
2024-03-17 23:32:03 +0530

    ✅ Added

--- index.js
+++ index.js
@@ -11,12 +11,12 @@ app.use("./routes/user_routes", userRouter);
 app.use("./routes/task_routes", taskRouter);
 
 //* Connect to the database //
-// const MONGO_URL = process.env.DB_CONNECT;
+const MONGO_URL = process.env.DB_CONNECT;
 mongoose
   .connect(
-    "mongodb+srv://mdferaj143:GYGh7rcqNt6B7Us7@cluster0.fmdxzhu.mongodb.net/TaskBuddy?retryWrites=true&w=majority&appName=Cluster0"
+    // "mongodb+srv://mdferaj143:GYGh7rcqNt6B7Us7@cluster0.fmdxzhu.mongodb.net/TaskBuddy?retryWrites=true&w=majority&appName=Cluster0"
 
-    // MONGO_URL
+    MONGO_URL
   )
   .then(() => console.log("Connected to Database ✨"))
   .catch((err) => console.log(err));
